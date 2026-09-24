b50c091a94cb7f98411cf01f725962e929edc9f8
JrDev <josemanuelramirezrengifo9@gmail.com>
2026-09-24 06:49:50 -0400

    Implement home route with HTML response
    Add a home route that returns a basic HTML page

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
 from fastapi import FastAPI, Response
 from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/home', tags=["Home"])
+def home(response: Response):
+    client_ip = response.client_host
+
+    return HTMLResponse(
+      """
+      <!DOCTYPE html>
+        <html lang="es">
+          <head>
+            <title>Say Hello</title>
+              <meta charset="utf-8">
+                <meta name="description" content="This server say Hello">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          </head>
+          <body>
+          </body>
+        </html>
+      """
+    )
